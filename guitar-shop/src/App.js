e5dd8427a380086266698b88a07ee2052c952fb0
Luka David <ldavi029@uottawa.ca>
2021-01-24 13:08:23 -0500

    Added component pages and cleared some old data

--- guitar-shop/src/App.js
+++ guitar-shop/src/App.js
@@ -1,9 +1,51 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link
+  } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Models from "./components/Models"; 
+import Contact from "./components/Contact"; 
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+			<li>
+              <Link to="/Models">Models</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+			<Route path="/models">
+            <Models />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ guitar-shop/src/components/Models.js
@@ -0,0 +1,9 @@
+import React, {Component } from 'react';
+
+class Models extends Component {
+	render(){
+		return <h2>Models</h2>;
+  }
+}
+
+export default Models;
