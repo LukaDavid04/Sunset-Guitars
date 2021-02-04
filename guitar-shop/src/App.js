71f70dc8b27bf9074948f9ab5c79253ec6aca394
Luka David <ldavi029@uottawa.ca>
2021-02-03 23:30:21 -0500

    Fixed images on the main page and added css properties

--- guitar-shop/src/App.js
+++ guitar-shop/src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <Router>
       <div>
-		<img src="https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2020/05/PRS-SE-P20E-Gear-Patrol-Lead-Full.jpg" alt="Second card with information"/> 
-		<img src="https://cdn.mos.cms.futurecdn.net/2ahx5EMCazvGZWA224XxFb.jpg" alt="First card with title"/> 
-		<img src="https://online.berklee.edu/takenote/wp-content/uploads/2020/07/learn_acoustic_blues_guitar_article_image.jpg" alt="Third card with information"/> 
+		<img class="MainImage" src="https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2020/05/PRS-SE-P20E-Gear-Patrol-Lead-Full.jpg" alt="Second card with information"/> 
+		<img class="MainImage" src="https://cdn.mos.cms.futurecdn.net/2ahx5EMCazvGZWA224XxFb.jpg" alt="First card with title"/> 
+		<img class="MainImage" src="https://online.berklee.edu/takenote/wp-content/uploads/2020/07/learn_acoustic_blues_guitar_article_image.jpg" alt="Third card with information"/> 
       </div>
     </Router>
   );
