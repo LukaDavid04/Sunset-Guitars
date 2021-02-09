9be7d1d492231eda0a1ab03f897e2a850ddec3b7
Luka David <ldavi029@uottawa.ca>
2021-02-08 23:11:08 -0500

    Adding titles to blockimage

--- guitar-shop/src/App.js
+++ guitar-shop/src/App.js
@@ -7,9 +7,7 @@ import BlockImage from './components/BlockImage';
 function App() {
   return (
     <Router>
-      <div>
 		<BlockImage/>
-      </div>
     </Router>
   );
 }
--- guitar-shop/src/components/BlockImage.js
+++ guitar-shop/src/components/BlockImage.js
@@ -1,10 +1,10 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 
 class BlockImage extends Component{
 	render() {
 		return <div>
 			<img class="MainImage" src="https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2020/05/PRS-SE-P20E-Gear-Patrol-Lead-Full.jpg" alt="Second card with information"/> 
-			<img class="MainImage" src="https://cdn.mos.cms.futurecdn.net/2ahx5EMCazvGZWA224XxFb.jpg" alt="First card with title"/> 
+			<img class="MainImage" src="https://images.squarespace-cdn.com/content/v1/5772af1029687f3cdf68994e/1591209125915-SUBV9APTFSMESDSO7ZN1/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/lr-baggs-acoustic-guitar-pickups-banner.jpg?format=1500w" alt="First card with title"/> 
 			<img class="MainImage" src="https://online.berklee.edu/takenote/wp-content/uploads/2020/07/learn_acoustic_blues_guitar_article_image.jpg" alt="Third card with information"/> 
 		</div>;
 	}
