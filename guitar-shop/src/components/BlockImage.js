38b74d8a17c55192559d5d97cead7a3044ed9f0b
Luka David <ldavi029@uottawa.ca>
2021-02-11 23:26:28 -0500

    Added text blocks to main page and updated css

--- guitar-shop/src/components/BlockImage.js
+++ guitar-shop/src/components/BlockImage.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class BlockImage extends Component{
 	render() {
 		return <div>
+			<link rel="preconnect" href="https://fonts.gstatic.com"></link><link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital@1&display=swap" rel="stylesheet"></link>
+			<div class="cont">
 			<img class="MainImage" src="https://hips.hearstapps.com/amv-prod-gp.s3.amazonaws.com/gearpatrol/wp-content/uploads/2020/05/PRS-SE-P20E-Gear-Patrol-Lead-Full.jpg" alt="Second card with information"/> 
-			<img class="MainImage" src="https://images.squarespace-cdn.com/content/v1/5772af1029687f3cdf68994e/1591209125915-SUBV9APTFSMESDSO7ZN1/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/lr-baggs-acoustic-guitar-pickups-banner.jpg?format=1500w" alt="First card with title"/> 
+			<div class="blockTextMain">Bad Alek's Guitars</div></div><div class="cont">
+			<img class="MainImage" src="https://cdn11.bigcommerce.com/s-yhkec2kugv/images/stencil/800x800/uploaded_images/001.jpg?t=1588890907" alt="First card with title"/> 
+			<div class="blockText">My name is Aleksandar motherfucking David and I am passionate about making some fucking nice guitars. I believe in squeezing every last molecule of quality out of the materials I buy and making some beautiful creations.</div></div><div class="cont">
 			<img class="MainImage" src="https://online.berklee.edu/takenote/wp-content/uploads/2020/07/learn_acoustic_blues_guitar_article_image.jpg" alt="Third card with information"/> 
-		</div>;
+			<div class="blockText">I am dedicated to crafting extraordinary musical instruments. I continually research the history, mechanics, dynamics and physics of the guitar in order to cultivate a deeper understanding of what makes it work and how I can continue to advance my instruments.</div></div><div class="cont">
+		</div></div>;
 	}
 }
 
